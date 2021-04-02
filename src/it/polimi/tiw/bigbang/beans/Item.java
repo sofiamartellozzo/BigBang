e248319828e0b172396fa29347b1531a3781c646
sofiamartellozzo <sofia.martellozzo@mail.polimi.it>
2021-04-02 16:53:16 +0200

    add Item bean

---
+++ src/it/polimi/tiw/bigbang/beans/Item.java
@@ -0,0 +1,42 @@
+package it.polimi.tiw.bigbang.beans;
+
+public class Item {
+	private int id;
+	private String name;
+	private String description;
+	private String category;
+	private String picture;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	
+	
+
+}
