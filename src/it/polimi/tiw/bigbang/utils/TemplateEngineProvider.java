7b3d9ebf09ed63fe8d4282f72cbd4a927344a3ed
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-04-02 16:05:56 +0200

    create provider for thymeleaf template engine

---
+++ src/it/polimi/tiw/bigbang/utils/TemplateEngineProvider.java
@@ -0,0 +1,21 @@
+package it.polimi.tiw.bigbang.utils;
+
+import javax.servlet.ServletContext;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+public class TemplateEngineProvider {
+	
+	public static TemplateEngine getTemplateEngine(ServletContext context) {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setSuffix(".html");
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		
+		return templateEngine;
+	}
+
+}
